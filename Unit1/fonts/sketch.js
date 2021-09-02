aed0aa28c04d9a9a90f380eb5614aa14a23b0971
LyndsiePhoto <laschlink.photo@gmail.com>
2021-09-02 10:20:37 -0500

    dug

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,14 +1,18 @@
-var font1;
+var font1, font2;
 
 function setup() {
   createCanvas(700, 700);
   font1 = loadFont ("assets/LuckiestGuy.ttf");
+  font2 = loadFont ("assets/Satisfy.ttf");
 }
 
 function draw() {
   background ("yellow");
   textFont (font1, 48);
-  textSize (90);
   text ("hello world", 75, 75);
+
+  textFont (font2, 48);
+  text ("Photo Chick", 75, 175);
+
 
 }
