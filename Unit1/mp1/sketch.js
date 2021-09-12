b8ef4fd028884974fd7c0d63d53654675dbb9b71
LyndsiePhoto <laschlink.photo@gmail.com>
2021-09-11 20:07:18 -0500

    go

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -211,6 +211,10 @@ function draw() {
     ellipse(width / 2 - 135, height / 2 - 40, 20, 20);
     //center of side button
 
+    textFont(font2);
+    textSize (24);
+    text("This is Ruby the Polaroid camera. Her superpower is her ability to capture the beauty of the sun in a single \n frame. She enjoys traveling the world with her human and capturing beautiful sunrise and sunset scenes to \n share with others throughout the world.", 5, 500);
+
 
   }
 
