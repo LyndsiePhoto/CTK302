4b54d588454702cce1d5153061b2d58227bfa7d3
LyndsiePhoto <laschlink.photo@gmail.com>
2021-09-30 09:10:13 -0500

    go

--- Unit2/button/sketch.js
+++ Unit2/button/sketch.js
@@ -1,5 +1,5 @@
-let state = 0 ;
-let mouseX = 0 ;
+let state = 0;
+
 
 function setup() {
   createCanvas(500, 500);
@@ -8,31 +8,31 @@ function setup() {
 function draw() {
 
   switch (state) {
-      case 0:
-      background('white');
+    case 0:
+      background('blue');
       text("case 0", 250, 250);
       break;
-       case 1:
+    case 1:
       background('grey');
       text("case 1", 250, 250);
       break;
-  case 2:
+    case 2:
       background('pink');
       text("case 2", 250, 250);
       break;
   }
 
+  fill('yellow');
+  rect(100, 100, 100, 100);
+
 }
 
 function mouseReleased() {
-  state++ ;
-  if (state > 2) {
-    state = 0 ;
-  }
-}
-
-
-if ((mouseX > 100) && (mouseX < 200) && (mouseY > 100) && (mouseY < 200)) {
+  if ((mouseX > 100) && (mouseX < 200) && (mouseY > 100) && (mouseY < 200)) {
     state++;
-    if (state > 2) state = 0;
+    if (state > 2) {
+      state = 0;
+    }
   }
+
+}
