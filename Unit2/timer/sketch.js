ed2ddcd37080b6a1fedfb5ca39634a5138d2496a
LyndsiePhoto <laschlink.photo@gmail.com>
2021-10-05 18:14:11 -0500

    timer

--- Unit2/timer/sketch.js
+++ Unit2/timer/sketch.js
@@ -9,29 +9,37 @@ function draw() {
 
   switch (state) {
     case 0:
-    background('yellow');
-    textSize (25);
-    text("Knock, knock.\n Who's there?", 100, height/2) ;
-    break;
+      background('yellow');
+      textSize(25);
+      text("Knock, knock.\n Who's there?", 100, height / 2);
+      timer++;
+      if (timer > 2 * 60) {
+        timer = 0;
+        state = 1;
+      }
+      break;
 
     case 1:
-    background('blue');
-    textSize (25);
-    text("Robin.\n Robin who?", 100, height/2) ;
-    break;
+      background('blue');
+      textSize(25);
+      text("Robin.\n Robin who?", 100, height / 2);
+      timer++;
+      if (timer > 3 * 60) {
+        timer = 0;
+        state = 2;
+      }
+        break;
 
-    case 2:
-    background('red');
-    textSize (25);
-    text("Robin you.\n Give me your money!", 100, height/2) ;
-    break;
-        }
-
-        timer ++ ;
-        if (timer > 3*60) {
+        case 2:
+        background('red');
+        textSize(25);
+        text("Robin you.\n Give me your money!", 100, height / 2);
+        timer++;
+        if (timer > 4 * 60) {
           timer = 0;
-          state ++;
-          if (state > 2) state = 0;
+          state = 2;
+        }
+          break;
         }
 
       }
