0bca058001ff9eb6c9f16b6ee3fa43a0955303de
LyndsiePhoto <laschlink.photo@gmail.com>
2021-11-21 16:50:49 -0600

    up

--- Unit4/mp4/sketch.js
+++ Unit4/mp4/sketch.js
@@ -43,7 +43,7 @@ function setup() {
 }
 
 function draw() {
-  image(image4, width, height, windowWidth, windowHeight);
+  image(image4, 1000, 3000, windowWidth, windowHeight);
   //background('red'); // light blue
 
   // the map command !!!!
