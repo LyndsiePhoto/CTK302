345e65f387b1b8dbd514fd259388534209362d1c
LyndsiePhoto <laschlink.photo@gmail.com>
2021-12-04 16:27:12 -0600

    yt

--- Unit4/mp5/sketch.js
+++ Unit4/mp5/sketch.js
@@ -91,7 +91,7 @@ function draw() {
   fill('black');
   textFont(font1, 30);
   textAlign(CENTER);
-  text("Help buddy collect syrup and Santa.", width / 2, 600, windowWidth - 200, windowHeight - 200);
+  text("Help buddy find Santa and syrup.", width / 2, 600, windowWidth - 200, windowHeight - 200);
 
 
   // Debugging information -- take this out when you're ready for production!
